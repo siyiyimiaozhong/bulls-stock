68fd1632c5a36dc4f92060c9626a5b6c0728e64e
siyiyimiaozhong <1440738649@qq.com>
2022-11-13 21:22:37 +0800

    entity模块创建

---
+++ bulls-stock-backend/bulls-stock-entity/src/main/java/com/siyi/bulls/stock/entity/base/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.siyi.bulls.stock.entity.base;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @Author: siyiyimiaozhong
+ * @Description:
+ * @Project: bulls-stock-backend
+ * @Package: com.siyi.bulls.stock.entity.base
+ * @ClassName: BaseEntity.java
+ * @CreateTime: 2022-11-13  21:18
+ * @Version: 1.0
+ */
+@Setter
+@Getter
+public class BaseEntity implements Serializable {
+
+    /**
+     * 唯一主键标识
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+}
