a93e9ee9c2f55d67ce559786a025a774e91ef854
siyiyimiaozhong <1440738649@qq.com>
2022-11-13 15:59:46 +0800

    提交代码生成器实现

---
+++ bulls-stock-backend/mybatis-generator/src/main/java/com/siyi/mybatis/generator/constant/GeneratorConstant.java
@@ -0,0 +1,28 @@
+package com.siyi.mybatis.generator.constant;
+
+/**
+ * @Author: siyiyimiaozhong
+ * @Description:
+ * @Project: bulls-stock-backend
+ * @Package: com.siyi.mybatis.generator.constant
+ * @CreateTime: 2022-10-26  23:38
+ * @Version: 1.0
+ */
+public class GeneratorConstant {
+    /**
+     * 数据库url
+     */
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/%s?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
+    /**
+     * 数据库名
+     */
+    public static final String DB_NAME = "bulls_stock";
+    /**
+     * 数据库用户名
+     */
+    public static final String USERNAME = "root";
+    /**
+     * 数据库密码
+     */
+    public static final String PASSWORD = "root";
+}
---
+++ bulls-stock-backend/mybatis-generator/src/main/java/com/siyi/mybatis/generator/startup/GeneratorApplication.java
@@ -0,0 +1,24 @@
+package com.siyi.mybatis.generator.startup;
+
+import com.siyi.mybatis.generator.custom.CustomCommentGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: siyiyimiaozhong
+ * @Project: bulls-stock-backend
+ * @Package: com.siyi.mybatis.generator.startup
+ * @CreateTime: 2022-10-24  22:59
+ * @Version: 1.0
+ */
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        List<String> tableNames = Arrays.asList("t_company", "t_trade_user");
+        String modelName = "trade.oauth";
+        CustomCommentGenerator generator = new CustomCommentGenerator(modelName, tableNames);
+        generator.generateCode();
+    }
+}
